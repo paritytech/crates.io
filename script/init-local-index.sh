3959012b72dc5127a3f2ea93615d8c1d3bb6af5a
joao-paulo-parity <joao-paulo@parity.io>
2022-11-11 13:39:24 -0300

    wip

--- script/init-local-index.sh
+++ script/init-local-index.sh
@@ -2,37 +2,37 @@
 
 set -e
 
-this_file="${BASH_SOURCE[0]}"
+this_file="$(realpath -s "${BASH_SOURCE[0]}")"
 this_dir="${this_file%/*}"
 project_root="${this_dir%/*}"
 
 cd "$project_root"
 
-# if [ -d tmp/index-bare ]; then
-#     echo tmp/index-bare already exists, exiting
-#     exit 0
-# fi
-#
-# mkdir -p tmp
-# rm -rf tmp/index-bare tmp/index-tmp
-#
-# echo "Initializing repository in tmp/index-bare..."
-# git init -q --bare --initial-branch=master tmp/index-bare
-#
-# echo "Creating temporary clone in tmp/index-tmp..."
-# git clone --depth 1 https://github.com/rust-lang/crates.io-index tmp/index-tmp
-# rm -rf tmp/index-tmp/.git
-# git init -q --initial-branch=master tmp/index-tmp
-# cd tmp/index-tmp
-# cat > config.json <<-EOF
-# {
-#   "dl": "http://localhost:8888/api/v1/crates",
-#   "api": "http://localhost:8888/"
-# }
-# EOF
+if [ -d tmp/index-bare ]; then
+    echo tmp/index-bare already exists, exiting
+    exit 0
+fi
+
+mkdir -p tmp
+rm -rf tmp/index-bare tmp/index-tmp
+
+echo "Initializing repository in tmp/index-bare..."
+git init -q --bare --initial-branch=master tmp/index-bare
+
+echo "Creating temporary clone in tmp/index-tmp..."
+git clone --depth 1 https://github.com/rust-lang/crates.io-index tmp/index-tmp
+rm -rf tmp/index-tmp/.git
+git init -q --initial-branch=master tmp/index-tmp
+cd tmp/index-tmp
+cat > config.json <<-EOF
+{
+  "dl": "http://localhost:8888/api/v1/crates",
+  "api": "http://localhost:8888/"
+}
+EOF
 git add .
 git commit -qm 'Initial commit'
-git remote set-url origin file://"$project_root"/tmp/index-bare
+git remote add origin file://"$project_root"/tmp/index-bare
 git push -q origin master -u > /dev/null
 cd "$project_root"
 
